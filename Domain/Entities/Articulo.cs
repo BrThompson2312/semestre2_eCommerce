89604586c2f85f68c3c0c97af9dbde0bc70fe68d
BrThompson2312 <brunogomez2312@gmail.com>
2024-09-11 22:44:04 -0300

    testing

---
+++ Domain/Entities/Articulo.cs
@@ -0,0 +1,17 @@
+namespace Entities
+{
+    public class Articulo
+    {
+        private static int id = 0;
+        private string nombre;
+        private string categoria;
+        private int precio;
+        public Articulo(string _nombre, string _categoria, int _precio)
+        {
+            id++;
+            nombre = _nombre;
+            categoria = _categoria;
+            precio = _precio;
+        }
+    }
+}
--- obj/Debug/net8.0/eCommerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/eCommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eCommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff4fda38c1e837f9a53ab6af8487ed0c5af39adb")]
 [assembly: System.Reflection.AssemblyProductAttribute("eCommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eCommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
