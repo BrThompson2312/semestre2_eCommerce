ff039b093b1d688e871701b3e9dee9cb7f878c0a
BrThompson2312 <brunogomez2312@gmail.com>
2024-10-04 11:48:30 -0300

    entrega v1

--- ejecutar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-dotnet exec /var/www/html/lenguajes/cSharp/eCommerce/Main/bin/Debug/net8.0/Main.dll
--- exec.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dotnet build Main.csproj
-clear
-dotnet exec /var/www/html/lenguajes/cSharp/eCommerce/Main/bin/Debug/net8.0/Main.dll
