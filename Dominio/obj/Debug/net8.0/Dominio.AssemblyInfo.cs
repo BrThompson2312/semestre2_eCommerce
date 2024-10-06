3f2e1497d54850cbde432f4278e25101a8352ce9
BrThompson2312 <brunogomez2312@gmail.com>
2024-10-06 12:07:31 -0300

    entrega 1.0.2

--- Dominio/obj/Debug/net8.0/Dominio.AssemblyInfo.cs
+++ Dominio/obj/Debug/net8.0/Dominio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a79b3dff3eeb3f8767aa13b595d80d925cd9592d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86d68bc48d2040232ec12c456a3758575ec0df06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
