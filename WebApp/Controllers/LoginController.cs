a75da7a91524ccd7a106e362eb6ff0baa3da4975
BrThompson2312 <brunogomez2312@gmail.com>
2024-10-31 13:44:22 -0300

    cambios 31/10/24

--- WebApp/Controllers/LoginController.cs
+++ WebApp/Controllers/LoginController.cs
@@ -4,9 +4,20 @@ namespace WebApp.Controllers
 {
     public class LoginController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Index(string Email, string Contrasenia)
+        { 
+            HttpContext.Session.SetString ("email", Email);
+            HttpContext.Session.SetString("contrasenia", Contrasenia);
+            return Redirect("/Usuario/index");
+        }
+        
+
     }
 }
--- WebApp/Controllers/UsuarioController.cs
+++ WebApp/Controllers/UsuarioController.cs
@@ -1,6 +1,7 @@
 ﻿using Dominio;
 using Dominio.Entidades;
 using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace WebApp.Controllers
 {
@@ -33,10 +34,63 @@ namespace WebApp.Controllers
             return View("index");
         }
 
-        public IActionResult AltaAdministrador(Usuario usuario)
+        [HttpGet]
+        public IActionResult AltaAdministrador()
         {
+            ViewBag.Usuarios = _sistema.Usuarios;
             return View();
         }
 
+        [HttpPost]
+        public IActionResult AltaAdministrador(Usuario usuario)
+        {
+            try
+            {
+                _sistema.AgregarUsuario(usuario);
+                return RedirectToAction("index", new { mensaje = "Se dio de alta el administrador en forma exitosa." });
+
+                
+            }
+            catch (Exception e)
+            {
+                ViewBag.mensaje = e.Message;
+                ViewBag.Usuarios = _sistema.ListadoAdministradores();
+            }
+            return View(usuario);
+        }
+
+
+        [HttpGet]
+        public IActionResult AltaCliente()
+        {
+            ViewBag.Usuarios = _sistema.Usuarios;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AltaCliente(Usuario usuario)
+        {
+            try
+            {
+                _sistema.AgregarUsuario(usuario);
+                return RedirectToAction("index", new { mensaje = "Se dio de alta el cliente en forma exitosa." });
+
+
+            }
+            catch (Exception e)
+            {
+                ViewBag.mensaje = e.Message;
+                ViewBag.Usuarios = _sistema.ListadoClientes();
+            }
+            return View(usuario);
+        }
+        
     }
 }
+
+
+
+
+
+
+   
